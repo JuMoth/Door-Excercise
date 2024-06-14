4c7f778aa59432f48e9dc226201219bf4a5b0958
JuMoth <tkdlqjqkd@gmail.com>
2024-06-14 19:55:51 +0900

    Feat: functional separation

--- 04_03.js
+++ 04_03.js
@@ -3,16 +3,26 @@
     // 현재 활성화된 아이템을 저장하고 있는 변수
     let currentItem;
 
+    // 활성화
+    function activate(elem) {
+        elem.classList.add('door-opened');
+        currentItem = elem;
+    }
+
+    // 비활성화
+    function inactivate(elem) {
+        elem.classList.remove('door-opened');
+    }
+
     const doorHandler = (e) => {
         const targetElem = e.target;
 
         if(currentItem) {
-            currentItem.classList.remove('door-opened');
+            inactivate(currentItem);
         }
 
-        if(targetElem.classList.contains('door-body')){
-            targetElem.parentNode.classList.add('door-opened');
-            currentItem = targetElem.parentNode;
+        if(targetElem.classList.contains('door-body')){ 
+            activate(targetElem.parentNode);
         }
     }
 
