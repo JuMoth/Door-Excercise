c1c3d27458abe293ef13cbdbb20c267f8d51de89
JuMoth <tkdlqjqkd@gmail.com>
2024-06-14 19:00:45 +0900

    first commit

---
+++ 04_03.js
@@ -0,0 +1,20 @@
+(function() {
+    const stageElem = document.querySelector('.stage');
+    // 현재 활성화된 아이템을 저장하고 있는 변수
+    let currentItem;
+
+    const doorHandler = (e) => {
+        const targetElem = e.target;
+
+        if(currentItem) {
+            currentItem.classList.remove('door-opened');
+        }
+
+        if(targetElem.classList.contains('door-body')){
+            targetElem.parentNode.classList.add('door-opened');
+            currentItem = targetElem.parentNode;
+        }
+    }
+
+    stageElem.addEventListener('click', doorHandler);
+})();
